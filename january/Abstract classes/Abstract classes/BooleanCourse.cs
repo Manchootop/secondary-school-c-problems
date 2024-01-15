83f296bc229a4ba5b2713f31dc00e46e181cd165
unknown <mariqn5000@gmail>
2024-01-15 18:52:43 +0200

    abstract classes

---
+++ january/Abstract classes/Abstract classes/BooleanCourse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    public class BooleanCourse : Course
+    {
+        private bool grade;
+        public BooleanCourse(string name, bool grade) : base(name)
+        {
+            this.grade = grade;
+        }
+
+        //тук вече методът има тяло
+        public override bool Passed()
+        {
+            return this.grade;
+        }
+    }
+}
---
+++ january/Abstract classes/Abstract classes/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    public class Project
+    {
+        private Course c1, c2, c3, c4;
+
+        public Project(Course c1, Course c2, Course c3, Course c4)
+        {
+            this.c1 = c1;
+            this.c2 = c2;
+            this.c3 = c3;
+            this.c4 = c4;
+        }
+        
+        public bool Passed()
+        {
+            return (c1.Passed() && c2.Passed() && c3.Passed() && c4.Passed()) || (!c1.Passed() && c2.Passed() && c3.Passed() && c4.Passed()) || (c1.Passed() && !c2.Passed() && c3.Passed() && c4.Passed()) || (c1.Passed() && c2.Passed() && !c3.Passed() && c4.Passed()) || (c1.Passed() && c2.Passed() && c3.Passed() && !c4.Passed());
+        }
+    }
+}
