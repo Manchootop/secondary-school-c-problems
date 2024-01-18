eecfa2d4ae0e94323d8fb8b3c359be4257678dc4
manchootop <mariqn5000@gmail.com>
2024-01-18 14:51:00 +0200

    solved problems with classes and interfaces

---
+++ january/Exercise - abstract classes/1. border control/Program.cs
@@ -0,0 +1,10 @@
+﻿string command = Console.ReadLine();
+
+while (command != "End")
+{
+    if (command.Split().Length == 2)
+    {
+        string model = command.Split()[0];
+        string id = command.Split()[1];
+    }
+}
---
+++ january/Exercise - abstract classes/1. border control/Robot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Robot : IPerson
+{
+    public string Model { get; set; }
+    public string Id { get; set; }
+    public string Name;
+    public Robot(string model, string id)
+    {
+        this.Model = model;
+        this.Id = id;
+    }
+
+    public void CheckIdentity(decimal id, decimal fakeId)
+    {
+        while (true)
+        {
+            if (id.Equals(fakeId))
+            {
+                Console.WriteLine(id);
+                break;
+            }
+            else
+                id /= 10;
+        }
+
+    }
+}
