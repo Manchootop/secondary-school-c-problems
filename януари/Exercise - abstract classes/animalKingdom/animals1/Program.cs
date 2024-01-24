76233838762e129045c1d5b3a6cf09a60db7d3ae
unknown <mariqn5000@gmail>
2024-01-24 13:11:39 +0200

    solved problems

---
+++ януари/Exercise - abstract classes/animalKingdom/animals1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace animals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cat kitten = new Cat("maria", 4);
+            Dog doggie = new Dog("fifi", 3);
+
+            Console.WriteLine("Cat: ");
+            Console.WriteLine(kitten.MakeNoise());
+            Console.WriteLine(kitten.MakeTrick()); 
+
+            Console.WriteLine("Dog: ");
+            Console.WriteLine(doggie.MakeNoise());
+            Console.WriteLine(doggie.MakeTrick()); 
+
+
+        }
+    }
+}
